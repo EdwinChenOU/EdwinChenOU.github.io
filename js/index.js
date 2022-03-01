215073ef3b48d067054cbb3834d4eaeb3498dfe9
Edwin Chen <70176021+EdwinChenOU@users.noreply.github.com>
2022-03-01 15:04:04 -0600

    simple fixes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
 
-navToggle/addEventListener('click', () => {
+navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
